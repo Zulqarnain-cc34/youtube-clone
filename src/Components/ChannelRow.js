52f4b1a69ce2594414354461c8bd0f6e63cae3fc
Zulqarnain123 <powerranger16918@gmail.com>
2020-08-28 02:53:10 +0500

    changes

---
+++ src/Components/ChannelRow.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../Components Css/ChannelRow.css";
+import Avatar from "@material-ui/core/Avatar";
+import VerifiedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
+function ChannelRow({
+  image,
+  channel,
+  verified,
+  subs,
+  noofvideos,
+  description,
+}) {
+  return (
+    <div className="channelrow">
+      <div className="channelrow_Avatar">
+        <Avatar className="channelRow_logo" src={image} alt={channel} />
+      </div>
+      <div className="channelrow_text">
+        <h4>
+          {channel}
+          {verified && <VerifiedIcon className="channelRow_verified" />}
+        </h4>
+        <p>
+          {subs} . {noofvideos}
+        </p>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ChannelRow;
--- src/Components/Searchpage.js
+++ src/Components/Searchpage.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "../Components Css/Searchpage.css";
 import TuneOutlinedicon from "@material-ui/icons/TuneOutlined";
+import ChannelRow from "./ChannelRow";
 
 function Searchpage() {
   return (
     <div className="searchpage">
       <div className="searchpage__filter">
         <TuneOutlinedicon />
-        <h2>Filter</h2>
+        <h2>FILTER</h2>
       </div>
+      <hr />
+      <ChannelRow
+        image="https://yt3.ggpht.com/a/AATXAJymy6xr9_W7GTkPW1ov2OyxzvNCOE-ff1d7VK36Cg=s48-c-k-c0xffffffff-no-rj-mo"
+        channel="Mosh Hamdani"
+        verified
+        subs="788k"
+        noofvideo={281}
+        description="React is really elegant in the way it is designed and compiled. Kudos to the facebook team, and of course Mosh who taught us!"
+      />
     </div>
   );
 }
